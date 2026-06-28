d18a24718668ed8b2f9b52876c5c180ce8bc5858
javascripter29 <2009.12illya@gmail.com>
2026-06-28 14:46:15 +0300

    fix deploy 2

--- src/utils/prisma.ts
+++ src/utils/prisma.ts
@@ -1,11 +1,14 @@
 import { PrismaClient } from "../generated/prisma/client"; 
 import { PrismaPg } from "@prisma/adapter-pg"; 
+import { withAccelerate } from '@prisma/extension-accelerate'
 const globalForPrisma = global as unknown as {
   prisma: PrismaClient; 
 }; 
 const adapter = new PrismaPg({
   connectionString: process.env.DATABASE_URL, 
 }); 
+
+
 const prisma =
   globalForPrisma.prisma ||
   new PrismaClient({
